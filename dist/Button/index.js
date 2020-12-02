67b9ed1a9ba7af8436ffe966b82e176460b7a67d
diegoocezar <diego.bernardino@amedigital.com>
2020-12-02 13:24:56 -0300

    test style

--- dist/Button/index.js
+++ dist/Button/index.js
@@ -7,12 +7,15 @@ exports.default = Button;
 
 var _react = _interopRequireDefault(require("react"));
 
+require("./styles.scss");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function Button(_ref) {
   var text = _ref.text,
       onClick = _ref.onClick;
   return /*#__PURE__*/_react.default.createElement("button", {
+    className: 'ds-button',
     onClick: onClick
   }, text);
 }
